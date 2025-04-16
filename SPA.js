a52445d1b5af75759f303cf4672a4929a2ef1714
tytyu073iop <fpm.biryuk@bsu.by>
2025-04-16 08:34:48 +0300

    correction of behaviour

--- SPA.js
+++ SPA.js
@@ -1,18 +1,11 @@
 "use strict";
 
-window.onhashchange=switchToStateFromURLHash;
-
 // текущее состояние приложения
 // это Model из MVC
 var SPAState={};
 
-// фотографии, которые можно просмотреть
-var photos={
-  1 : { image:"Hilu3.jpg", comment:"собака Шарик" },
-  2 : { image:"Retriever3.jpg", comment:"собака Барбос" }
-};
+const buttonsIds = ["Plants", "Main"];
 
-// вызывается при изменении закладки УРЛа
 // а также при первом открытии страницы
 // читает новое состояние приложения из закладки УРЛа
 // и обновляет ВСЮ вариабельную часть веб-страницы
@@ -20,22 +13,19 @@ var photos={
 // это упрощённая реализация РОУТИНГА - автоматического выполнения нужных
 // частей кода в зависимости от формы URLа
 // "роутинг" и есть "контроллер" из MVC - управление приложением через URL
-function switchToStateFromURLHash() {
-  var URLHash=window.location.hash;
+function switchToState(state) {
 
   // убираем из закладки УРЛа решётку
   // (по-хорошему надо ещё убирать восклицательный знак, если есть)
-  var stateStr=URLHash.substr(1);
 
-  if ( stateStr != "" ) { // если закладка непустая, читаем из неё состояние и отображаем
-    var parts=stateStr.split("_")
-    SPAState={ pagename: parts[0] }; // первая часть закладки - номер страницы
-  } else {
-    SPAState={pagename:'Main'}; // иначе показываем главную страницу
-  }
+  SPAState.pagename = state
 
   console.log('Новое состояние приложения:');
   console.log(SPAState);
+
+  for (let id of buttonsIds) {
+    document.getElementById(id).style.display = id === state ? "none" : "block";
+  }
 
   // обновляем вариабельную часть страницы под текущее состояние
   // это реализация View из MVC - отображение состояния модели в HTML-код
@@ -82,4 +72,4 @@ function switchToStateFromURLHash() {
   document.getElementById('IPage').innerHTML=pageHTML;
 }
 
-switchToStateFromURLHash();
+switchToState('Main');
